1d81fe3e3784be6c274c79212146090a27d8bf90
Usama Khan <usamaarshadkhan@gmail.com>
2022-05-19 22:43:25 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Usama Khan on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
