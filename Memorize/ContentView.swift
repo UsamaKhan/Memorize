e2816ca2ba3a2d2dd64cd8ed6a444cc7818db310
Usama Khan <usamaarshadkhan@gmail.com>
2022-05-21 22:55:49 -0500

    Completed Lecture 2 Updated code to display cards in a grid and added functionality to update count of cards being viewed. Created a separate view for cards that can be modified with different faces as well as being face up or down.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,20 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    @State var emojiCount = 20
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) {
+                        emoji in CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                removeButton
+                Spacer()
+                addButton
+            }
+            .font(.largeTitle)
+            .padding([.top, .leading, .trailing])
+        }
+        .padding(.all)
+    }
+    
+    var addButton: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.rectangle.portrait")
+        }
+    }
+    var removeButton: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.rectangle.portrait")
+        }
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, world!")
+            let card = RoundedRectangle(cornerRadius: 10)
+            if isFaceUp {
+                card.fill(.white)
+                card.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                card
+            }
         }
-        .padding()
-        .foregroundColor(.red)
-
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
