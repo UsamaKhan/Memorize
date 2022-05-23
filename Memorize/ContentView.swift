b4f9626cbad28c472e6db241fe9728ec2412c5d3
Usama Khan <usamaarshadkhan@gmail.com>
2022-05-23 00:32:57 -0500

    Completed Lecture 3 Updated app to implement MVVM architecture. Created a MemoryGame model and EmojiMemoryGame view model to start implementing game logic. Reverted ContentView to realign with lecture code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,96 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = [
-        ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"],
-        ["🏳️", "🏴", "🏴‍☠️", "🏁", "🚩", "🇺🇳", "🇺🇸", "🇨🇦", "🇬🇧", "🇵🇰", "🇮🇳", "🇨🇳", "🇯🇵", "🇰🇷", "🇦🇺", "🇪🇸", "🇿🇦", "🇶🇦", "🇸🇦", "🇳🇵", "🇲🇻", "🇹🇷", "🏳️‍🌈", "🏳️‍⚧️"],
-        ["😀", "😆", "😂", "🥲", "😉", "😍", "🤪", "😎", "😒", "😭", "🤯", "😱", "🫠", "😬", "😴", "😵", "🤮", "😷", "🤠", "🤡", "💩", "👽", "🤖", "🎃"],
-        ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐔", "🐥", "🦆", "🦅", "🐙", "🦀", "🐟", "🦈", "🐊"]
-    ]
+//    emojis:
+//        vehicles = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+//        flags = ["🏳️", "🏴", "🏴‍☠️", "🏁", "🚩", "🇺🇳", "🇺🇸", "🇨🇦", "🇬🇧", "🇵🇰", "🇮🇳", "🇨🇳", "🇯🇵", "🇰🇷", "🇦🇺", "🇪🇸", "🇿🇦", "🇶🇦", "🇸🇦", "🇳🇵", "🇲🇻", "🇹🇷", "🏳️‍🌈", "🏳️‍⚧️"]
+//        emotes = ["😀", "😆", "😂", "🥲", "😉", "😍", "🤪", "😎", "😒", "😭", "🤯", "😱", "🫠", "😬", "😴", "😵", "🤮", "😷", "🤠", "🤡", "💩", "👽", "🤖", "🎃"]
+//        animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐔", "🐥", "🦆", "🦅", "🐙", "🦀", "🐟", "🦈", "🐊"]
     
-    @State var theme = 0
-    @State var emojiCount = 16
-    @State var themeColor = Color.red
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    @State var emojiCount = 20
     
     var body: some View {
         VStack {
-            Text("Memorize!")
-                .font(.title)
-                .foregroundColor(.blue)
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
-                    ForEach(emojis[theme].shuffled()[0..<emojiCount], id: \.self) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) {
                         emoji in CardView(content: emoji)
                             .aspectRatio(2/3, contentMode: .fit)
                     }
                 }
             }
-            .foregroundColor(themeColor)
-            HStack {
-                removeButton
-                Spacer()
-                themeSelector
-                Spacer()
-                addButton
-            }
-            .font(.largeTitle)
-            .padding([.top, .leading, .trailing])
+            .foregroundColor(.red)
         }
-        .padding(.horizontal)
-    }
-    
-    var addButton: some View {
-        Button {
-            if emojiCount < emojis[theme].count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.rectangle.portrait")
-        }
-    }
-    var removeButton: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.rectangle.portrait")
-        }
-    }
-    var themeSelector: some View {
-        Menu {
-            Button {
-                theme = 0
-                emojiCount = Int.random(in: 4...16)
-                themeColor = Color.red
-            } label: {
-                Label("Transportation", systemImage: "car")
-            }
-            Button {
-                theme = 1
-                emojiCount = Int.random(in: 4...16)
-                themeColor = Color.purple
-            } label: {
-                Label("Flags", systemImage: "flag")
-            }
-            Button {
-                theme = 2
-                emojiCount = Int.random(in: 4...16)
-                themeColor = Color.orange
-            } label: {
-                Label("Emotes", systemImage: "face.smiling")
-            }
-            Button {
-                theme = 3
-                emojiCount = Int.random(in: 4...16)
-                themeColor = Color.green
-            } label: {
-                Label("Animals", systemImage: "hare")
-            }
-        } label: {
-            Text("Themes")
-        }
-        .font(.title2)
+        .padding(.all)
     }
 }
 
